8fd0c3d143223a0b6b3c5481060805a28da4c8f6
Vadim Skidanov <vadim.skidanovs@gmail.com>
2022-05-26 11:06:25 +0100

    add new font. header height change

---
+++ src/api/apiConfig.js
@@ -0,0 +1,9 @@
+const apiConfig = {
+  api_url:
+    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=a4299efd747250310a54f11d044965e8&page=1",
+  img_path: "https://image.tmdb.org/t/p/w1280",
+  search_api:
+    'https://api.themoviedb.org/3/search/movie?api_key=a4299efd747250310a54f11d044965e8&query="',
+};
+
+export default apiConfig;
