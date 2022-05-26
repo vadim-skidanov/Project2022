a41a5d7a5b269170d3ec9b3b6e87bb405dcac8f6
Vadim Skidanov <vadim.skidanovs@gmail.com>
2022-05-26 23:26:30 +0100

    movie info styling change. Header links style change.

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -3,10 +3,14 @@ import classes from "./Movie.module.css";
 const Movie = (props) => {
   return (
     <div className={classes.movie}>
-      <img className={classes["movie-img"]} src={props.poster} alt="" />
+      <img
+        className={classes["movie-img"]}
+        src={props.poster}
+        alt={`${props.title} movie poster`}
+      />
       <div className={classes["movie-info"]}>
-        <div className={classes.title}>{props.title}</div>
-        <span className="raiting">{props.rating}</span>
+        <div className={classes["movie-title"]}>{props.title}</div>
+        <span className={classes["movie-rating"]}>{props.rating}</span>
       </div>
     </div>
   );
