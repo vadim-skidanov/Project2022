3fc4ae42de831d9ce75cb4e4205cfdcdb6c98b6e
Vadim Skidanov <vadim.skidanovs@gmail.com>
2022-05-28 22:43:36 +0100

    input selection colors changed

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -2,11 +2,7 @@ import { forwardRef } from "react";
 import classes from "./Input.module.css";
 
 const Input = forwardRef((props, ref) => {
-  return (
-    <>
-      <input className={classes.input} ref={ref} {...props.input} />
-    </>
-  );
+  return <input className={classes.input} ref={ref} {...props.input} />;
 });
 
 export default Input;
