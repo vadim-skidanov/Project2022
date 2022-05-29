c44b8d018a31f804238253976203dcf3c81f2fa3
Vadim Skidanov <vadim.skidanovs@gmail.com>
2022-05-29 11:50:20 +0100

    search bar code refactore

--- src/components/Movies/MovieSearch/MovieSearch.js
+++ src/components/Movies/MovieSearch/MovieSearch.js
@@ -1,19 +1,7 @@
-import { useState } from "react";
-import SearchIcon from "../../../assets/SearchIcon/SearchIcon";
 import SearchBar from "../../UI/SearchBar/SearchBar";
 import classes from "./MovieSearch.module.css";
 
 const MovieSearch = () => {
-  const [toggleSearchBar, setToggleSearchBar] = useState(false);
-
-  const onSearchLogoClick = () => {
-    setToggleSearchBar((prevState) => !prevState);
-  };
-
-  const searchBarClasses = toggleSearchBar
-    ? `${classes["search-input"]} ${classes["expand"]}`
-    : classes["search-input"];
-
   const onSearch = (e) => {
     e.preventDefault();
   };
@@ -22,9 +10,8 @@ const MovieSearch = () => {
 
   return (
     <div className={classes.search}>
-      <SearchIcon onIconClick={onSearchLogoClick} />
-      <form onSubmit={onSearch}>
-        <SearchBar className={searchBarClasses} placeholder={placeholderText} />
+      <form className={classes["search-form"]} onSubmit={onSearch}>
+        <SearchBar placeholder={placeholderText} />
       </form>
     </div>
   );
--- src/components/UI/SearchBar/SearchBar.js
+++ src/components/UI/SearchBar/SearchBar.js
@@ -1,10 +1,30 @@
+import { useState } from "react";
+import SearchIcon from "./SearchIcon/SearchIcon";
+import classes from "./SearchBar.module.css";
+
 const SearchBar = (props) => {
+  const [toggleSearchBar, setToggleSearchBar] = useState(false);
+
+  const searchBarClasses = toggleSearchBar
+    ? `${classes["search-input"]} ${classes["expand"]}`
+    : classes["search-input"];
+
+  const onSearchLogoClick = () => {
+    setToggleSearchBar((prevState) => !prevState);
+  };
+
   return (
-    <input
-      type="search"
-      className={props.className}
-      placeholder={props.placeholder}
-    />
+    <>
+      <label htmlFor="search">
+        <SearchIcon onIconClick={onSearchLogoClick} />
+      </label>
+      <input
+        id="search"
+        type="search"
+        className={searchBarClasses}
+        placeholder={props.placeholder}
+      />
+    </>
   );
 };
 
