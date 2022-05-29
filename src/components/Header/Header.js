839ee6db8983133a66c61f603c9fa3a50b6bcf5c
Vadim Skidanov <vadim.skidanovs@gmail.com>
2022-05-29 21:49:03 +0100

    work on bug

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,15 @@
 import { useContext } from "react";
 import MovieContext from "../../store/movie-context/movie-context";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Navigation from "./Navigation/Navigation";
 import classes from "./Header.module.css";
 
 const Header = () => {
   const movieCtx = useContext(MovieContext);
+  const location = useLocation();
 
   const resetSearchCtx = () => {
+    // if(location.pathname === "/home")
     movieCtx.setSearchTerm("");
     movieCtx.resetMovie("");
     document.location.reload();
