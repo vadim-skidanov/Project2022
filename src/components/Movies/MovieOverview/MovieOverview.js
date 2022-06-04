f4b48b234af6cac02298d2efe1269de1790e8d65
Vadim Skidanov <vadim.skidanovs@gmail.com>
2022-06-04 12:58:54 +0100

    add backdrop

--- src/components/Movies/MovieOverview/MovieOverview.js
+++ src/components/Movies/MovieOverview/MovieOverview.js
@@ -9,20 +9,26 @@ const MovieOverview = (props) => {
   };
 
   return (
-    <section className={classes["movie-overview-section"]}>
-      <div className={classes["movie-img"]}>
-        <img src={props.poster} alt="" />
-      </div>
-
-      <div className={classes["movie-overview"]}>
-        <h2 className={classes["movie-title"]}>{props.title}</h2>
-        <p className={classes["movie-plot"]}>{props.plot}</p>
-        <div className={classes["movie-release-date"]}>
-          Release date: <span>{props.release_date}</span>
+    <>
+      <div className={classes.backdrop} />
+      <section className={classes["movie-overview-section"]}>
+        <div className={classes["movie-img"]}>
+          <img src={props.poster} alt="" />
         </div>
-        <button onClick={resetSelectedMovie} className={classes.close}></button>
-      </div>
-    </section>
+
+        <div className={classes["movie-overview"]}>
+          <h2 className={classes["movie-title"]}>{props.title}</h2>
+          <p className={classes["movie-plot"]}>{props.plot}</p>
+          <div className={classes["movie-release-date"]}>
+            Release date: <span>{props.release_date}</span>
+          </div>
+          <button
+            onClick={resetSelectedMovie}
+            className={classes.close}
+          ></button>
+        </div>
+      </section>
+    </>
   );
 };
 
